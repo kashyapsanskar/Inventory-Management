c9c0dd85aa1e15a5e66759295d644782909297d0
SANSKAR KASHYAP <kashyapsanskar95@gmail.com>
2024-10-29 20:53:27 +0530

    Created Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import Image from "next/image";
+import DashboardWrapper from "./Dashboard/DashboardWrapper";
+import Dashboard from "./Dashboard/page";
 
-export default function Home() {
-  return (
-   
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        hello
-       </main>
+export default function Home(){
+
+  
+  return(<>
+   <Dashboard/>
+  
+   </>
   );
+
 }
